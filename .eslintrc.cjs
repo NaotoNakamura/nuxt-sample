2c0f54011dbf6e62ba7efbc28bbb54cfe9580a15
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2023-12-28 17:15:54 +0900

    eslintとprettierのセットアップ

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "@nuxtjs/eslint-config-typescript",
+    "@vue/prettier",
+    "plugin:vue/vue3-recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      files: ["**/*.vue"],
+      rules: { "vue/multi-word-component-names": "off" },
+    },
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};
